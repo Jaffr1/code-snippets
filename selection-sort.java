374d33940d452c8f7d0e5640152c9035089dec9d
Jaffr1 <150064181+Jaffr1@users.noreply.github.com>
2026-04-10 08:51:25 +0530

    Create selection-sort.java

---
+++ selection-sort.java
@@ -0,0 +1,24 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 4, 3, 2};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
